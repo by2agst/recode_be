e5baec98ea64f738881a2a37fd6e4b74d9755f2d
Robby Agustinus <by2agst@gmail.com>
2020-11-19 09:27:08 +0700

    refresh tokken

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -1,2 +1,36 @@
+const _ = require('lodash')
+const jwt = require('jsonwebtoken')
+
 module.exports = {
+  async refreshToken (ctx) {
+    try {
+	    const { jwt } = _.assign(ctx.request.body)
+      const { id } = await this.verify(jwt)
+      const user = await strapi.query('user', 'users-permissions').findOne({ id })
+      ctx.send({
+        jwt: strapi.plugins['users-permissions'].services.jwt.issue({
+          id: user.id
+        })
+      })
+    } catch (e) {
+      console.log(e)
+      return ctx.badRequest(null, 'Invalid token')
+    }
+  },
+
+  verify (token) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(
+        token,
+        _.get(strapi.plugins, ['users-permissions', 'config', 'jwtSecret']),
+        { ignoreExpiration: true },
+        (err, tokenPayload = {}) => {
+          if (err) {
+            return reject(new Error('Invalid token.'))
+          }
+          resolve(tokenPayload)
+        }
+      )
+    })
+  }
 }
